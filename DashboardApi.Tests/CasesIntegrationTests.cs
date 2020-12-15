2ee3dc1280f00ceb206241e73230c51de1a40611
Muhammed Özkan <muhammedozkan@windowslive.com>
2020-12-15 22:47:41 +0300

    A few more tests have been added.
    Co-authored-by Muhammed Ozkan <muhammedozkan@windowslive.com>
    Co-authored-by Pinar Erdem <pinarerdem96@gmail.com>

--- DashboardApi.Tests/CasesIntegrationTests.cs
+++ DashboardApi.Tests/CasesIntegrationTests.cs
@@ -12,15 +12,33 @@ namespace DashboardApi.Tests
         {
             _factory = factory;
         }
-        
-        
+
+
         [Theory]
         [InlineData("cases")]
+        [InlineData("cases/topCountries/?sortby=")]
         [InlineData("cases/topCountries/?sortby=confirmed_today")]
+        [InlineData("cases/topCountries/?sortby=deaths_today")]
+        [InlineData("cases/topCountries/?sortby=recovered_today")]
+        [InlineData("cases/topCountries/?sortby=confirmed_change")]
+        [InlineData("cases/topCountries/?sortby=deaths_change")]
+        [InlineData("cases/topCountries/?sortby=recovered_change")]
+        [InlineData("cases/?country=")]
         [InlineData("cases/?country=Turkey")]
+        [InlineData("cases/?country=turkey")]
+        [InlineData("cases/?country=TURKEY")]
+        [InlineData("cases/?country=US")]
+        [InlineData("cases/?country=uS")]
+        [InlineData("cases/?country=us")]
         [InlineData("cases/topCountries")]
         [InlineData("cases/totalCases")]
+        [InlineData("cases/totalCases/?country=")]
         [InlineData("cases/totalCases/?country=Turkey")]
+        [InlineData("cases/totalCases/?country=turkey")]
+        [InlineData("cases/totalCases/?country=TURKEY")]
+        [InlineData("cases/totalCases/?country=uS")]
+        [InlineData("cases/totalCases/?country=US")]
+        [InlineData("cases/totalCases/?country=us")]
         public async Task CasesControllerEndpointsReturnsSuccess(string url)
         {
             var client = _factory.CreateClient();
@@ -28,6 +46,30 @@ namespace DashboardApi.Tests
             var response = await client.GetAsync(url);
 
             response.EnsureSuccessStatusCode();
+        }
+
+
+        //test 404 not found
+        [Theory]
+        [InlineData("cases1")]
+        [InlineData("cases/?country=US1")]
+        public async Task CasesControllerEndpointsReturnsFail(string url)
+        {
+            var client = _factory.CreateClient();
+
+            var response = await client.GetAsync(url);
+
+
+            try
+            {
+                response.EnsureSuccessStatusCode();
+            }
+            catch (System.Net.Http.HttpRequestException e)
+            {
+                Assert.Equal("Response status code does not indicate success: 404 (Not Found).", e.Message);
+            }
+            
+            
         }
     }
 }
