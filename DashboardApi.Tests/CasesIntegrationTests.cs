e2f519ad250a5be35621b071b8eb0c09b7f646ff
Muhammed Özkan <muhammedozkan@windowslive.com>
2020-12-27 14:20:22 +0300

    sayfa contentine göre test caseleri eklendi.

--- DashboardApi.Tests/CasesIntegrationTests.cs
+++ DashboardApi.Tests/CasesIntegrationTests.cs
@@ -1,6 +1,9 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
 using Xunit;
+
 
 namespace DashboardApi.Tests
 {
@@ -63,6 +66,7 @@ namespace DashboardApi.Tests
             try
             {
                 response.EnsureSuccessStatusCode();
+                
             }
             catch (System.Net.Http.HttpRequestException e)
             {
@@ -71,5 +75,86 @@ namespace DashboardApi.Tests
             
             
         }
+
+
+        
+        [Theory]
+
+        [InlineData("cases/totalCases")]
+        [InlineData("cases/totalCases/?country=")]
+        [InlineData("cases/totalCases/?country=Turkey")]
+        [InlineData("cases/totalCases/?country=turkey")]
+        [InlineData("cases/totalCases/?country=TURKEY")]
+        [InlineData("cases/totalCases/?country=uS")]
+        [InlineData("cases/totalCases/?country=US")]
+        [InlineData("cases/totalCases/?country=us")]
+        public async Task CasesControllerEndpointsTotalCaseContentTest(string url)
+        {
+            var client = _factory.CreateClient();
+
+            var response = await client.GetAsync(url);
+
+            var responseString = JsonConvert.DeserializeObject<DashboardApi.Models.CovidCaseTotal>(await response.Content.ReadAsStringAsync());
+
+
+            Assert.InRange(responseString.ConfirmedTotal, int.MinValue, int.MaxValue);
+            Assert.InRange(responseString.DeathsTotal, int.MinValue, int.MaxValue);
+            Assert.InRange(responseString.RecoveredTotal, int.MinValue, int.MaxValue);
+
+        }
+
+
+
+        [Theory]
+
+        [InlineData("cases/?country=", "worldwide")]
+        [InlineData("cases/?country=Turkey", "Turkey")]
+        [InlineData("cases/?country=turkey", "Turkey")]
+        [InlineData("cases/?country=TURKEY", "Turkey")]
+        [InlineData("cases/?country=US", "US")]
+        [InlineData("cases/?country=uS", "US")]
+        [InlineData("cases/?country=us", "US")]
+        public async Task CasesControllerEndpointsCasesCountryBaseContentTest(string url,string country)
+        {
+            var client = _factory.CreateClient();
+
+            var response = await client.GetAsync(url);
+
+            List<DashboardApi.Models.CovidCase> responseString;
+            responseString= JsonConvert.DeserializeObject<List<DashboardApi.Models.CovidCase>>(await response.Content.ReadAsStringAsync());
+           
+            Assert.Equal(country, responseString[0].CountryRegion);
+           
+
+        }
+
+        [Theory]
+        [InlineData("cases/topCountries")]
+        [InlineData("cases/topCountries/?sortby=")]
+        [InlineData("cases/topCountries/?sortby=confirmed_today")]
+        [InlineData("cases/topCountries/?sortby=deaths_today")]
+        [InlineData("cases/topCountries/?sortby=recovered_today")]
+        [InlineData("cases/topCountries/?sortby=confirmed_change")]
+        [InlineData("cases/topCountries/?sortby=deaths_change")]
+        [InlineData("cases/topCountries/?sortby=recovered_change")]
+        public async Task CasesControllerEndpointsCasesTopCountryBaseContentTest(string url)
+        {
+            var client = _factory.CreateClient();
+
+            var response = await client.GetAsync(url);
+
+            List<DashboardApi.Models.CovidCase> responseString;
+            responseString = JsonConvert.DeserializeObject<List<DashboardApi.Models.CovidCase>>(await response.Content.ReadAsStringAsync());
+
+            Assert.InRange(responseString[0].ConfirmedChange, int.MinValue, int.MaxValue);
+            Assert.InRange(responseString[0].ConfirmedToday, int.MinValue, int.MaxValue);
+            Assert.InRange(responseString[0].DeathsChange, int.MinValue, int.MaxValue);
+            Assert.InRange(responseString[0].DeathsToday, int.MinValue, int.MaxValue);
+            Assert.InRange(responseString[0].RecoveredChange, int.MinValue, int.MaxValue);
+            Assert.InRange(responseString[0].RecoveredToday, int.MinValue, int.MaxValue);
+
+        }
+
+
     }
 }
